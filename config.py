3ce69a810988cb160640cf5473095aa3e2e39389
Yonko9770 <121147878+Yonko9770@users.noreply.github.com>
2023-02-24 11:04:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,7 @@ if os.path.exists(".env"):
 # Necessary Vars
 API_ID = int(os.getenv("API_ID", "21055228"))
 API_HASH = os.getenv("API_HASH", "1fac2ebdf068d0f4d9e619abf49db06c")
-SESSION = os.getenv("SESSION", "AQCLEaBFTWRd8O3nvOEBQNwMKFQRdyl9dUxryIJMbvSQFTF1ft2CgLf_2T4R8hZyunVR-rV_RPNeZ3BzpMuheUE9CR1uVv9g88mVj4RWZp42hsGBfwQE4OU-OaLvlB2llVIGagYJWFBd5BQ6jPf0VpmOpNYYSbWl8M09avEju5wCfXPLKkmwmLy1MqiRjcoG6JGqzy72F2IdZzyOwnW4rr81bfpcVc6GBA99CLYAeWWf6Uf0MmvFvvb0LNRUufnTGcrbqjj4UcF69Mijrqys1hHGJVJHA9_7o42ifcr3oa_HbXzw4udGysd_bM8xQxYMNw734a7iQOnkhNcWYUzmRSbVAAAAAVsoCHAA
-")
+SESSION = os.getenv("SESSION", "AQCLEaBFTWRd8O3nvOEBQNwMKFQRdyl9dUxryIJMbvSQFTF1ft2CgLf_2T4R8hZyunVR-rV_RPNeZ3BzpMuheUE9CR1uVv9g88mVj4RWZp42hsGBfwQE4OU-OaLvlB2llVIGagYJWFBd5BQ6jPf0VpmOpNYYSbWl8M09avEju5wCfXPLKkmwmLy1MqiRjcoG6JGqzy72F2IdZzyOwnW4rr81bfpcVc6GBA99CLYAeWWf6Uf0MmvFvvb0LNRUufnTGcrbqjj4UcF69Mijrqys1hHGJVJHA9_7o42ifcr3oa_HbXzw4udGysd_bM8xQxYMNw734a7iQOnkhNcWYUzmRSbVAAAAAVsoCHAA")
 OWNER = os.getenv("OWNER", "5477247654")
 SUPPORT = os.getenv("SUPPORT", "Xd_Bot_Support")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS","5700727404").split()))
