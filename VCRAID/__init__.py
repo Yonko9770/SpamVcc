d1678dbdd9900873a2d8d804160d4b2b83f2d036
Yonko9770 <121147878+Yonko9770@users.noreply.github.com>
2023-02-24 11:19:13 +0530

    Update __init__.py

--- VCRAID/__init__.py
+++ VCRAID/__init__.py
@@ -2,10 +2,10 @@ from config import *
 from pytgcalls import PyTgCalls
 from pyrogram import Client, filters
 
-API_ID = 
-API_HASH =
-SESSION = 
-SUDO_USERS = 
+API_ID = "21055228"
+API_HASH = "1fac2ebdf068d0f4d9e619abf49db06c"
+SESSION = "AQCLEaBFTWRd8O3nvOEBQNwMKFQRdyl9dUxryIJMbvSQFTF1ft2CgLf_2T4R8hZyunVR-rV_RPNeZ3BzpMuheUE9CR1uVv9g88mVj4RWZp42hsGBfwQE4OU-OaLvlB2llVIGagYJWFBd5BQ6jPf0VpmOpNYYSbWl8M09avEju5wCfXPLKkmwmLy1MqiRjcoG6JGqzy72F2IdZzyOwnW4rr81bfpcVc6GBA99CLYAeWWf6Uf0MmvFvvb0LNRUufnTGcrbqjj4UcF69Mijrqys1hHGJVJHA9_7o42ifcr3oa_HbXzw4udGysd_bM8xQxYMNw734a7iQOnkhNcWYUzmRSbVAAAAAVsoCHAA"
+SUDO_USERS = "5700727404"
 
 contact_filter = filters.create(
     lambda _, __, message: (message.from_user and message.from_user.is_contact)
